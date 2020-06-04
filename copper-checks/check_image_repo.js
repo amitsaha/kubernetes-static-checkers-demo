f4118b766e44b796a9e48024e9d8f31d7c364940
Amit Saha <amitsaha.in@gmail.com>
2020-06-04 10:58:14 +1000

    update

--- copper-checks/check_image_repo.js
+++ copper-checks/check_image_repo.js
@@ -2,8 +2,8 @@ $$.forEach(function($){
     if ($.kind === 'Deployment') {
         $.spec.template.spec.containers.forEach(function(container) {
             var image = new DockerImage(container.image);
-            if (image.tag === 'latest') {
-                errors.add_error('no_latest',"latest is used in " + $.metadata.name, 1)
+            if (image.registry.lastIndexOf('my-company.com/') != 0) {
+                errors.add_error('no_company_repo',"Image " + $.metadata.name + " is not from my-company.com repo", 1)
             }
         });
     }
